e3cc38a6d81047854482e64ff58fe4254d83ff15
Mariya <mariyabaig0106@gmail.com>
2024-02-23 13:38:07 +0530

    css reoved

--- build/popup.js
+++ build/popup.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   var processButton = document.getElementById("processButton");
+  var outputDiv = document.getElementById("output");
 
   processButton.addEventListener("click", function () {
-    console.log("Button clicked!");
+    // Logic to process text goes here
+    // You can use chrome.tabs.query() or chrome.tabs.sendMessage() to communicate with content scripts
+    // Example:
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { command: "process" },
+        function (response) {
+          outputDiv.innerHTML = response.result;
+        }
+      );
+    });
   });
 });
