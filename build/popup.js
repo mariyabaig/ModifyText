e92acfd4919d312f2ae59c096a1a6de8c42c5e6d
mariyabaig <mariyabaig0106@gmail.com>
2024-03-03 15:37:19 +0530

    refactored

--- build/popup.js
+++ build/popup.js
@@ -2,20 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   var processButton = document.getElementById("processButton");
 
   processButton.addEventListener("click", function () {
-    // Send a message to the content script to grab text from the screen
+    console.log("Process button clicked");
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       console.log("Tabs:", tabs);
+
       chrome.tabs.sendMessage(
         tabs[0].id,
-        { command: "grabText" },
+        { command: "grabHTML" },
         function (response) {
           console.log("Response from content script:", response);
-          // Handle the response from the content script
+
           if (chrome.runtime.lastError) {
             console.error(chrome.runtime.lastError.message);
-          } else if (response && response.text) {
-            console.log("Text grabbed from the screen:", response.text);
-            // You can update the UI or perform further actions here
+          } else if (response && response.htmlContent) {
+            console.log("HTML content of the webpage:", response.htmlContent);
           } else {
             console.error("Unexpected response:", response);
           }
