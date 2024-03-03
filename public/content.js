eeb7c04ab9a4d4a484d7fe896fd292879d92106b
mariyabaig <mariyabaig0106@gmail.com>
2024-03-03 15:38:59 +0530

    refcatored

--- public/content.js
+++ public/content.js
@@ -1,12 +1,19 @@
 // Listen for messages from the popup script
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    console.log("Message received in content script:", message);
+    
     // Check if the message contains the command to grab HTML content
     if (message && message.command === "grabHTML") {
+        console.log("Command to grab HTML content received");
+
         // Extract HTML content from the entire webpage
         const htmlContent = document.documentElement.outerHTML;
+        
+        console.log("HTML content extracted from the webpage:", htmlContent);
+
         // Send the HTML content back to the popup script
         sendResponse({ htmlContent: htmlContent });
+    } else {
+        console.log("Unexpected message received:", message);
     }
-    // Return true to indicate that sendResponse will be called asynchronously
-    return true;
 });
